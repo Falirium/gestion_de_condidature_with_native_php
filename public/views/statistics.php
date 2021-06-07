b3ec15131b41fc999d830f2e55d081eb4ee341fd
ilovehacking92 <mafarfafaoua@insea.ac.ma>
2021-06-07 19:25:14 +0100

    Add :  statistics values

--- public/views/statistics.php
+++ public/views/statistics.php
@@ -1,20 +1,33 @@
+<?php
+require __DIR__ . "/../../vendor/autoload.php";
+
+$dossierDoa = \App\models\dossier\DossierDaoFactory::getDossierDaoFactory("mysql");
+
+$nbrDossiers = $dossierDoa->nombreDossiers();
+$nbrDossiersArchives = $dossierDoa->nombreDossiersArchives();
+$nbrDossiersActives = $dossierDoa->nombreDossiersActives();
+$montantTotal = $dossierDoa->montantTotal()['sum'];
+
+?>
+
+
 <div class="row h-100">
      <div class="col h-100 d-flex flex-column border-right">
           <div class="display-5 ml-3">Nombre de dossier</div>
           
-          <p class="stat-numbers mt-2 mb-0 text-center">5417£</p>
+          <p class="stat-numbers mt-2 mb-0 text-center"><?php echo $nbrDossiers; ?></p>
           
      </div>
      <div class="col h-100 d-flex flex-column border-right">
           <div class="display-5 ml-3">Montant total</div>
-          <p class="stat-numbers mt-2 mb-0 text-center">5417£</p>
+          <p class="stat-numbers mt-2 mb-0 text-center"><?php echo $montantTotal." DH"; ?></p>
      </div>
      <div class="col h-100 d-flex flex-column border-right">
           <div class="display-5 ml-3">Dossier archivé</div>
-          <p class="stat-numbers mt-2 mb-0 text-center">5417£</p>
+          <p class="stat-numbers mt-2 mb-0 text-center"><?php echo $nbrDossiersArchives; ?></p>
      </div>
      <div class="col h-100 d-flex flex-column">
           <div class="display-5 ml-3">Dossier activé</div>
-          <p class="stat-numbers mt-2 mb-0 text-center">5417£</p>
+          <p class="stat-numbers mt-2 mb-0 text-center"><?php echo $nbrDossiersActives; ?></p>
      </div>
 </div>
